12a9bbf1c655f016ff72b18fd1aeb8a8a6bc15a6
BalionIV <kathever98@gmail.com>
2026-06-05 20:37:30 +0100

    Fixed images not loading

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ ENV = 'DEVELOPPEMENT'
 if ENV == "DEVELOPPEMENT":
     DB_HOST     = "localhost"
     DB_USER     = "root"
-    DB_PASSWORD = "Yani2003@"
+    DB_PASSWORD = "admin123"
     DB_NAME     = "adlis"
 else: 
     DB_HOST     = "serveur_debergement"  #a changer
